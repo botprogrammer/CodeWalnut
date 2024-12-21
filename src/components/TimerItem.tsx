1ba09637e89e47b7d80dff7895cb061858d85042
pranav-royal-brothers <pranav.goswami@royalbrothers.com>
2024-12-21 14:25:35 +0530

    feat: Consolidated modal code

--- src/components/TimerItem.tsx
+++ src/components/TimerItem.tsx
@@ -4,10 +4,10 @@ import { Timer } from '../types/timer';
 import { formatTime } from '../utils/time';
 import { useTimerStore } from '../store/useTimerStore';
 import { toast } from 'sonner';
-import { EditTimerModal } from './EditTimerModal';
 import { TimerAudio } from '../utils/audio';
 import { TimerControls } from './TimerControls';
 import { TimerProgress } from './TimerProgress';
+import TimerModal from './common/TimerModal';
 
 interface TimerItemProps {
   timer: Timer;
@@ -126,7 +126,7 @@ export const TimerItem: React.FC<TimerItemProps> = ({ timer }) => {
         </div>
       </div>
 
-      <EditTimerModal
+      <TimerModal
         isOpen={isEditModalOpen}
         onClose={() => setIsEditModalOpen(false)}
         timer={timer}
